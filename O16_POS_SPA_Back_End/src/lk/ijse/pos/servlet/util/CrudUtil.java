8547947050945f6662976a58a48e7609db6adb2f
chathushkasavinda <chathushkasavinda18@gmail.com>
2023-09-10 16:02:01 +0530

    implements the util package componets

--- O16_POS_SPA_Back_End/src/lk/ijse/pos/servlet/util/CrudUtil.java
+++ O16_POS_SPA_Back_End/src/lk/ijse/pos/servlet/util/CrudUtil.java
@@ -1,7 +1,29 @@
 package lk.ijse.pos.servlet.util;
 
+import lk.ijse.pos.servlet.listners.MyListener;
+import org.apache.commons.dbcp2.BasicDataSource;
+
 import javax.servlet.ServletContext;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class CrudUtil {
-    static ServletContext servletContext = List
+    static ServletContext servletContext = MyListener.getServletContext();
+    static BasicDataSource dbcp = (BasicDataSource) servletContext.getAttribute("dbcp");
+
+    public static <T> T setQuery(String query,Object... args) throws SQLException {
+        try (Connection connection = dbcp.getConnection()){
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            for (int i = 0; i < args.length; i++) {
+                preparedStatement.setObject(i+1,args[i]);
+            }
+
+            if (query.startsWith("SELECT")||query.startsWith("select")){
+                return (T) preparedStatement.executeQuery();
+            }else {
+                return (T) (Boolean) (preparedStatement.executeUpdate()>0);
+            }
+        }
+    }
 }
